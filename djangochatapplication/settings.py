4b569f9eaa191e86ab064430ea05e033e48f0b25
arpansahu <asahu@gammastack.com>
2022-03-25 22:36:41 +0530

    first commit

--- djangochatapplication/settings.py
+++ djangochatapplication/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -154,3 +155,5 @@ TORTOISE_INIT = {
         "models": ["chat.tortoise_models"]
     }
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
