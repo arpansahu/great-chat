7049cf004dda6409334b3265947729cc5034ac3a
arpansahu <asahu@gammastack.com>
2022-03-25 23:16:45 +0530

    first commit

--- djangochatapplication/asgi.py
+++ djangochatapplication/asgi.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 import django
+
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochatapplication.settings')
 django.setup()
 
 from channels.auth import AuthMiddlewareStack
@@ -18,7 +22,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochatapplication.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
