ee2732933006e32aef3dd9977f13ab176976ed35
arpansahu <asahu@gammastack.com>
2022-03-25 23:34:02 +0530

    first commit

--- djangochatapplication/settings.py
+++ djangochatapplication/settings.py
@@ -146,7 +146,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [config('REDIS_URL')],
         },
     },
 }
